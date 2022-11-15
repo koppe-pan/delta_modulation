44aac700918fad18a43e9643d05978dbf271a0ae
koppe_pan <takas3141@gmail.com>
2022-11-15 15:13:52 +0900

    Use Environment Variable

--- src/create_data_part.py
+++ src/create_data_part.py
@@ -2,9 +2,11 @@ import scipy.io.wavfile as wavfile
 import numpy as np
 import os
 
-minus =  0b0 #BitArray('0b01') # 1
-plus = 0b1 #BitArray('0b10') # 2
-#zero = 0b00 #BitArray('0b00') # 0
+SR = int(os.environ.get('SR', '16000'))
+threshold = int(os.environ.get('THRESHOLD', '1000'))
+sample_per_part = int(os.environ.get('SAMPLE_PER_PART', '10000'))
+minus = os.environ.get('MINUS', '0').lower() in ('true', '1', 't')
+plus = os.environ.get('PLUS', '1').lower() in ('true', '1', 't')
 
 def encode_dm(source, scale, threshold):
     stream = []
@@ -32,16 +34,11 @@ for wf in wavlist:
     print("file: {}".format(wf))
     fs, data = wavfile.read("data/wav/{}".format(wf))
     data = data.astype(float)
-
-    SR = 16000
-    threshold = 1000
-
     scale = fs//SR
 
     stream = encode_dm(source=data, scale=scale, threshold=threshold)
     bn, _ = os.path.splitext(wf)
 
-    sample_per_part = 10000
     number_of_parts = len(data)//sample_per_part
     for num in range(number_of_parts):
         with open("data/binary/{}_SR{}_threshold{}_part{}".format(bn, SR, threshold, num),"wb") as f:
